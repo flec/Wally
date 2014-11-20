82bd06f92f8e99248f52e8dc5f1c225dc8a02250
flec <mfuellemann@gmail.com>
2014-11-20 18:31:50 +0100

    Created functions for channel and red extraction

--- matlab/extractRed.m
+++ matlab/extractRed.m
@@ -1,15 +1,7 @@
-img = imread('../img/WallyWembley.png'); %Load image into matrix X
-red = img(:,:,1); % Red channel
-green = img(:,:,2); % Green channel
-blue = img(:,:,3); % Blue channel
-a = zeros(size(img, 1), size(img, 2)); % zeros array
-greenBlue = greatestArray(green, blue); % get greatest of green and blue
-redOnly = greaterThan(red, greenBlue, 2); % get real red values
-redOnly = medfilt2(redOnly, [3,3]); % use median
-just_red = cat(3, redOnly, a, a); % create image from red only
-figure, imshow(just_red), title('Red colors') % show red image
-figure, imshow(img), title('Original') % show original
-
-
-
+function [ red ] = extractRed( r, g, b )
+% This function retuens the red channel with only the values that are red.
+% This means that red is at least twice as large as blue or green
+gb = greatestArray(g, b); % get greatest of green and blue
+red = greaterThan(r, gb, 2); % get real red values
+end
 
