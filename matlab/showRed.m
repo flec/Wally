f5047df1e4d64b1d6c87dc53b85262367aea69aa
joschi03 <joschi0391@gmail.com>
2014-11-21 15:04:39 +0100

    wembley cropped

--- matlab/showRed.m
+++ matlab/showRed.m
@@ -4,9 +4,5 @@ a = zeros(size(img, 1), size(img, 2)); % zeros array
 redOnly = extractRed(red, green, blue);
 redOnly = medfilt2(redOnly, [3,3]); % use median
 just_red = cat(3, redOnly, a, a); % create image from red only
-figure, imshow(just_red), title('Red colors') % show red image
 figure, imshow(img), title('Original') % show original
-
-
-
-
+figure, imshow(just_red), title('Red colors') % show red image
--- matlab/showRedWhite.m
+++ matlab/showRedWhite.m
@@ -4,5 +4,5 @@ img = imread('../img/WallyWembleyCropped.png'); %Load image into matrix
 [r, g, b] = extractWhite(red, green, blue);
 redCombined = greatestArray(r, extractRed(red, green, blue));
 redWhite = cat(3, redCombined, g, b); % create image from red only
-figure, imshow(redWhite), title('Red and white colors') % show white image
 figure, imshow(img), title('Original') % show original
+figure, imshow(redWhite), title('Red and white colors') % show white image
