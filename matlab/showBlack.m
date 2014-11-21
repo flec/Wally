948a657c4f322f927d0eb1a3f662670684e90b79
joschi03 <joschi0391@gmail.com>
2014-11-21 14:38:48 +0100

    showBlack

--- matlab/showBlack.m
+++ matlab/showBlack.m
@@ -1,9 +1,6 @@
 clear;
 img = imread('../img/WallyWembley.png'); %Load image into matrix
 [red, green, blue] = extractColorChannels(img);
-red(3,3)
-green(3,3)
-blue(3,3)
 [r, g, b] = extractBlack(red, green, blue);
 black = cat(3, r, g, b); % create image from red only
 
