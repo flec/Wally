5998a97cba3682678fe1e67b68989c79260b53d0
flec <mfuellemann@gmail.com>
2014-12-03 18:43:50 +0100

    added hough circle

--- matlab/showRedWhite.m
+++ matlab/showRedWhite.m
@@ -4,6 +4,7 @@ img = imread('../img/WallyWembleyCropped.png'); %Load image into matrix
 [r1, g1, b1] = extractWhite(red, green, blue);
 [r2, g2, b2] = extractRed(red, green, blue);
 [r, g, b] = greatestRGB(r1, g1, b1, r2, g2, b2);
+[r, g, b] = dilateErode(r, g, b);
 redWhite = cat(3, r, g, b); % create image from red only
 figure, imshow(img), title('Original') % show original
 figure, imshow(redWhite), title('Red and white colors') % show white image
