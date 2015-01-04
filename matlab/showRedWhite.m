4e76c4f9dfb7df7ed05826a57b037d037e55da2b
flec <mfuellemann@gmail.com>
2015-01-04 13:24:51 +0100

    Fixed white threshold for t-shirt detection

--- matlab/showRedWhite.m
+++ matlab/showRedWhite.m
@@ -1,7 +1,7 @@
 clear;
 img = imread('../img/WallyWembleyCropped.png'); %Load image into matrix
 [red, green, blue] = extractColorChannels(img);
-[r1, g1, b1] = extractWhite(red, green, blue);
+[r1, g1, b1] = extractWhite(red, green, blue, 0.8);
 [r2, g2, b2] = extractRed(red, green, blue);
 [r, g, b] = greatestRGB(r1, g1, b1, r2, g2, b2);
 redWhite = cat(3, r, g, b); % create image from red only
